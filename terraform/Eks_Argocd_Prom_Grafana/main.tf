d4df726a658d91d8568463bde55c459f6094291e
Vikash Kumar <Kvikash668@gmail.com>
2026-04-12 00:41:32 +0530

    EKS2

--- terraform/Eks_Argocd_Prom_Grafana/main.tf
+++ terraform/Eks_Argocd_Prom_Grafana/main.tf
@@ -1,4 +1,21 @@
 ########################################
+# TERRAFORM SETTINGS
+########################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+########################################
 # PROVIDER
 ########################################
 
@@ -7,10 +24,8 @@ provider "aws" {
 }
 
 ########################################
-# DATA
+# USE DEFAULT VPC
 ########################################
-
-data "aws_caller_identity" "current" {}
 
 data "aws_vpc" "default" {
   default = true
@@ -29,7 +44,7 @@ data "aws_subnets" "default" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.8.5"
 
   cluster_name    = "test"
   cluster_version = "1.29"
@@ -124,30 +139,7 @@ resource "aws_eks_addon" "ebs_csi" {
 }
 
 ########################################
-# ALB CONTROLLER IRSA (FIXED MODULE)
-########################################
-
-module "alb_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-  version = "6.4.0"
-
-  role_name = "AmazonEKSLoadBalancerControllerRole"
-
-  attach_load_balancer_controller_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn = module.eks.oidc_provider_arn
-
-      namespace_service_accounts = [
-        "kube-system:aws-load-balancer-controller"
-      ]
-    }
-  }
-}
-
-########################################
-# KUBERNETES & HELM PROVIDERS
+# CONNECT HELM TO EKS
 ########################################
 
 data "aws_eks_cluster" "cluster" {
@@ -156,14 +148,6 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(
-    data.aws_eks_cluster.cluster.certificate_authority[0].data
-  )
-  token = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
@@ -177,71 +161,9 @@ provider "helm" {
 }
 
 ########################################
-# SERVICE ACCOUNT (REQUIRED FOR IRSA)
-########################################
-
-resource "kubernetes_service_account" "alb_sa" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-
-    annotations = {
-      "eks.amazonaws.com/role-arn" = module.alb_irsa.iam_role_arn
-    }
-  }
-}
-
-########################################
-# ALB CONTROLLER INSTALL (HELM)
-########################################
-
-resource "helm_release" "alb_controller" {
-  name       = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-
-  depends_on = [
-    module.eks,
-    module.alb_irsa,
-    kubernetes_service_account.alb_sa
-  ]
-
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_name
-  }
-
-  set {
-    name  = "region"
-    value = "us-east-1"
-  }
-
-  set {
-    name  = "vpcId"
-    value = data.aws_vpc.default.id
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-}
-
-########################################
 # OUTPUTS
 ########################################
 
 output "cluster_name" {
   value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = data.aws_eks_cluster.cluster.endpoint
 }
